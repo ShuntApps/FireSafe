750847ad7c92d6c0fd82b5f1488436838d008662
Shunt <45630953+ShuntApps@users.noreply.github.com>
2019-01-18 11:05:20 +0000

    FireControllerUpdate
    Eelectrical Fire controller updated

---
+++ Assets/Scripts/electricalFireOff.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class electricalFireOff : MonoBehaviour {
+
+	
+
+   private void OnEnable()
+    {
+        NewEventManager.StartListening("fireOut", manageFire);
+    }
+
+    private void OnDisable()
+    {
+        NewEventManager.StopListening("fireOut", manageFire);
+    }
+
+	public void manageFire()
+	{
+		GetComponent<FireCheck>().enabled=true;
+	}
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<FireCheck>().enabled=false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/powerCutOff.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class powerCutOff : MonoBehaviour {
+
+	public Material mat;
+
+	// Use this for initialization
+	void Start () {
+		mat=GetComponent<Material>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	[ContextMenu("turnOffPower")]
+	public void turnOffPower()
+	{
+		mat.SetColor("_EmissionColor", Color.grey);
+		NewEventManager.TriggerEvent("powerOff");
+	}
+}
